13e3ecb5774110841413c1489c536482a13bccf7
Your Name <arturo.webrek@chilapa.mx>
2014-08-22 15:33:22 -0500

    a

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'tickets',
+			'database'  => 'fay',
 			'username'  => 'root',
-			'password'  => '12345',
+			'password'  => 'mysqlf4y',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
